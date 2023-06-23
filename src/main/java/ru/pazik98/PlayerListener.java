b8aff87d6987f28e4d8457c69f055ff049541166
pazik98 <nefedow98@mail.ru>
2023-06-23 04:27:36 +0300

    Implemented simple event processing logic
    	-	API version changed to 1.19.1

--- src/main/java/ru/pazik98/PlayerListener.java
+++ src/main/java/ru/pazik98/PlayerListener.java
@@ -1,4 +1,39 @@
 package ru.pazik98;
 
-public class PlayerListener {
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.logging.Logger;
+
+public class PlayerListener implements Listener {
+
+    private Logger logger = Bukkit.getLogger();
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            Material material = e.getMaterial();
+            Block block = e.getClickedBlock();
+
+            // Check for making farmland
+            if (isHoe(material) && isEarth(block.getType())) {
+
+                logger.warning("creating entity!");
+            }
+        }
+    }
+
+    private boolean isHoe(Material m) {
+        return m.equals(Material.WOODEN_HOE) || m.equals(Material.STONE_HOE) || m.equals(Material.IRON_HOE) ||
+                m.equals(Material.GOLDEN_HOE) || m.equals(Material.DIAMOND_HOE) || m.equals(Material.NETHERITE_HOE);
+    }
+
+    private boolean isEarth(Material m) {
+        return m.equals(Material.GRASS_BLOCK) || m.equals(Material.DIRT_PATH) || m.equals(Material.DIRT);
+    }
 }
--- src/main/java/ru/pazik98/plugin/HardcorePlanting.java
+++ src/main/java/ru/pazik98/plugin/HardcorePlanting.java
@@ -1,12 +1,13 @@
 package ru.pazik98.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.pazik98.PlayerListener;
 
 public class HardcorePlanting extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
     }
 
     @Override
